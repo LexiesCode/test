d8e5d5aa3df91bc968d02232aad2dd5393abbac6
LexiesCode <35810389+LexiesCode@users.noreply.github.com>
2018-02-06 17:31:28 +0300

    Редактирование отступов

--- html-element-collection/audioplayer/audioplayer.js
+++ html-element-collection/audioplayer/audioplayer.js
@@ -9,77 +9,77 @@ const nextTrack = controls.getElementsByClassName('fa-forward')[0];
 const previousTrack = controls.getElementsByClassName('fa-backward')[0];
 
 const trackList = [
-	{
-	title : 'LA Chill Tour',
-	src : 'mp3/LA Chill Tour.mp3'
-	}, 
-	{
-	title : 'This is it band',
-	src : 'mp3/This is it band.mp3'
-	},
-	{
-	title : 'LA Fusion Jam',
-	src : 'mp3/LA Fusion Jam.mp3'
-	}
+  {
+    title: 'LA Chill Tour',
+    src: 'mp3/LA Chill Tour.mp3'
+  }, 
+  {
+    title: 'This is it band',
+    src: 'mp3/This is it band.mp3'
+  },
+  {
+    title: 'LA Fusion Jam',
+    src: 'mp3/LA Fusion Jam.mp3'
+  }
 ];
 
 const trackTitle = audioplayer.getElementsByClassName('title')[0];
 let currentTrack = 0;
 
 function playCurrentTrack() {
-	audio.src = trackList[currentTrack].src;
-	trackTitle.title = trackList[currentTrack].title;
-	}
+  audio.src = trackList[currentTrack].src;
+  trackTitle.title = trackList[currentTrack].title;
+  }
 
 function changeTrack() {
-		audio.pause();
-		playCurrentTrack()
-		if(audioplayer.classList.contains('play')) {
-		audio.play();
-		}
+  audio.pause();
+  playCurrentTrack()
+  if(audioplayer.classList.contains('play')) {
+    audio.play();
+  }
 }
 
 btnPlayPause.onclick = () => {
-	if (audioplayer.classList.contains('play')) {
-		audio.pause();
-	} else {
-		audio.play();			
-	}
-	audioplayer.classList.toggle('play');
+  if (audioplayer.classList.contains('play')) {
+    audio.pause();
+  } else {
+    audio.play();			
+  }
+  audioplayer.classList.toggle('play');
 }
 
 /*btnPlay.onclick = () => {
-	audio.play();
-	audioplayer.classList.toggle('play');
+  audio.play();
+  audioplayer.classList.toggle('play');
 };
 btnPause.onclick = () => {
-	audio.pause();
-	audioplayer.classList.toggle('play');
+  audio.pause();
+  audioplayer.classList.toggle('play');
 };
 */
-	
+
 btnStop.onclick = () => {
-	audio.pause();
-	audio.currentTime = 0;
-	audioplayer.classList.remove('play');
+  audio.pause();
+  audio.currentTime = 0;
+  audioplayer.classList.remove('play');
 }
-	
+
 nextTrack.onclick = () => {       
-	currentTrack++;
-	if(currentTrack === trackList.length) {
-		currentTrack = 0;
-	}
-	changeTrack();
+  currentTrack++;
+  if(currentTrack === trackList.length) {
+    currentTrack = 0;
+  }
+  changeTrack();
 }
 
 previousTrack.onclick = () => {      
-	currentTrack--;
-	if(currentTrack < 0) {
-		currentTrack = trackList.length - 1;
-	}
-	changeTrack();
+  currentTrack--;
+  if(currentTrack < 0) {
+    currentTrack = trackList.length - 1;
+  }
+  changeTrack();
 }
-		
+
 audio.onended = () => {      
-	audioplayer.classList.remove('play');
+  audioplayer.classList.remove('play');
 }
